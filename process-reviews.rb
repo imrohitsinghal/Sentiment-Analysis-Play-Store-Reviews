903d667a8683a5ada19649c35e9e906ebeda4622
Rohit Singhal <singhalr@thoughtworks.com>
2017-08-13 23:00:52 +0530

    Updating logic for finding patters

--- process-reviews.rb
+++ process-reviews.rb
@@ -1,27 +1,32 @@
 require 'sentimental'
 require 'CSV'
 require_relative 'features/app_features.rb'
+require 'nokogiri'
+require 'mustache'
 
 $csv2 = CSV.open("negative_reviews.csv", "wb")
+$csv3 = CSV.open("functionality_reviews.csv", "wb")
+$csv3 << ['area', 'review']
+
 analyzer = Sentimental.new
 analyzer.load_defaults
 analyzer.threshold = 0.1
 
-def extract_words(string)
-  string.to_s.downcase.scan(/([\w']+|\S{2,})/).flatten
-end
-
 def affected_features string
-  extract_words(string).each do |word|
-    if FEATURES.include?(word)
-      print word + "\n"
+  FEATURES.each do |keyword|
+    if string.include?keyword
+      $csv3 << [keyword,string]
+      break
     end
   end
 end
 
 CSV.foreach("reviews_file.csv") do |row|
   if analyzer.sentiment row[1] != 'positive'
-      $csv2 << [row[1]]
-      affected_features row[1]
+    $csv2 << [row[1]]
+    affected_features row[1]
   end
 end
+
+
+
