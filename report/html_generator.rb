e797fa4a36a49d0c70da42a706a16b32330bc53f
Rohit Singhal <singhalr@thoughtworks.com>
2017-08-28 11:12:10 +0530

    Formatting HTML Output

--- report/html_generator.rb
+++ report/html_generator.rb
@@ -10,7 +10,7 @@ class ProduceHtml
   def build_html_table data
     html = Builder::XmlMarkup.new(:indent => 2)
     html.table {
-      html.tr { data[0].keys.each { |key| html.th(key) } }
+      html.tr { data[0].keys.each { |key| html.td(key) } }
       data.each { |row| html.tr { row.values.each { |value| html.td(value) } } }
     }
   end
@@ -19,6 +19,8 @@ class ProduceHtml
     table_data = build_html_table @array_of_hashes.sort_by! { |hsh| hsh[:area] }
     html_file = File.read(__dir__+"/result.html")
     html_file= html_file.gsub("#TABLE#", table_data)
+    html_file = html_file.gsub(" <td:area/>","<td><strong>Area</strong></td>")
+    html_file = html_file.gsub("<td:feedback/>","<td><strong>Review</strong></td>")
     File.open(__dir__ + "/review_report.html", 'w') { |f| f.write(html_file) }
   end
 
