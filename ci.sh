7e4f7f33b06869f13a4f783c9c019d67a6a0016a
rohsin <rohit.singhal@vuclip.com>
2017-11-23 10:18:30 +0530

    CI script to run all the rake tasks in Jenkins

--- ci.sh
+++ ci.sh
@@ -1,7 +1,29 @@
-#!/bin/bash -l
+#!/usr/bin/env bash
+set -e
 
 #Ensure all required gems are installed
 bundle install
 
-# Finally run the required tasks with rake
-bundle exec rake $*
+
+CURRENT_DIR=`pwd`
+
+#[ -z "$JAVA_HOME" ] && echo "JAVA_HOME is NOT SET AS AN ENVIRONMENT VARIABLE. Set it first then rerun the script" && exit 1;
+
+sleep 2
+
+cd $CURRENT_DIR
+echo "CURRENT_DIR - " $CURRENT_DIR
+
+date=`expr $(date +"%d") - 1`
+echo "*** FETCHING FOR PREVIOUS DAY ***"
+rake PACKAGE_NAME=com.vuclip.viu REVIEW_DATE=$date  reviews:run:all
+sleep 2
+
+echo "*** ANALYSING THE FEEDBACKS ***"
+rake PACKAGE_NAME=com.vuclip.viu  reviews:run:output
+sleep 2
+
+
+echo "*** ARTIFACTS GENERATED ***"
+
+
